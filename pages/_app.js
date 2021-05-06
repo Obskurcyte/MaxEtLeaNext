bca2969fa4d6017c224f9841d0c749ae2636d057
tdavray <theodore.d.avray@efrei.net>
2021-05-06 12:21:51 +0200

    Contact Form Sendinblue

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,7 @@ import {AppProvider} from "../components/context/AppContext";
 import React from "react";
 import { ApolloProvider } from '@apollo/client/react';
 import client from "../components/ApolloClient";
+import Head from 'next/head'
 
 const rootReducer = combineReducers({
   product: productReducer,
@@ -30,13 +31,26 @@ const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 function MyApp({ Component, pageProps }) {
   return(
+
+    <React.Fragment>
+          <Head>
+      <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-KV59DL8');`}} />
+    </Head>
+    <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KV59DL8" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
+
     <AppProvider>
       <ApolloProvider client={client}>
-  <Provider store={store}>
-    <Component {...pageProps} />
-  </Provider>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
       </ApolloProvider>
     </AppProvider>
+  </React.Fragment>
+    
   )
 }
 
